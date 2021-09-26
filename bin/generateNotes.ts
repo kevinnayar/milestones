bc1f3d94c471a03d85057abd08d5deef2a2d5304
kevinnayar <kevin.nayar@gmail.com>
2021-09-25 23:45:07 -0500

    change note creation script

--- bin/generateNotes.ts
+++ bin/generateNotes.ts
@@ -1,31 +1,28 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
-function getFileComments(text: string, commentMap: { [k: string]: string[] }): string[] {
+function insertFileCommentsInMap(rawText: string, mutableCommentMap: { [k: string]: string[] }) {
   const identifier = '// @notes';
-  const comments = [];
-  const lines: string[] = text.split('\n');
+  const lines: string[] = rawText.split('\n');
 
-  for (const _line of lines) {
-    const line = _line.trim();
-    if (line.trim().startsWith(identifier)) {
+  for (const l of lines) {
+    const line = l.trim();
+    if (line.startsWith(identifier)) {
       const regex = /\[(.*?)\]/;
       const namespace = regex.exec(line)[1];
       const comment = line.replace(`${identifier}[${namespace}] `, '');
 
-      if (commentMap[namespace]) {
-        commentMap[namespace].push(comment);
+      if (mutableCommentMap[namespace]) {
+        mutableCommentMap[namespace].push(comment);
       } else {
         // eslint-disable-next-line no-param-reassign
-        commentMap[namespace] = [comment];
+        mutableCommentMap[namespace] = [comment];
       }
     }
   }
-
-  return comments;
 }
 
-async function getDirContents(dirPath: string, commentMap: { [k: string]: string[] }) {
+function getDirContentsRecursively(dirPath: string, mutableCommentMap: { [k: string]: string[] }) {
   const files = fs.readdirSync(dirPath);
 
   for (const file of files) {
@@ -33,51 +30,53 @@ async function getDirContents(dirPath: string, commentMap: { [k: string]: string
     const isDir = fs.lstatSync(filePath).isDirectory();
     const isFile = Boolean(!isDir && filePath.match('.ts'));
 
-    if (isFile) getFileComments(fs.readFileSync(filePath).toString(), commentMap);
-    if (isDir) await getDirContents(filePath, commentMap);
+    if (isFile) insertFileCommentsInMap(fs.readFileSync(filePath).toString(), mutableCommentMap);
+    if (isDir) getDirContentsRecursively(filePath, mutableCommentMap);
   }
 }
 
-function createNotes(commentMap: { [k: string]: string[] }): string {
+function writeNotesToFile(outputFile: string, mutableCommentMap: { [k: string]: string[] }) {
   let notes = '';
-  const keys = Object.keys(commentMap).sort();
+  const keys = Object.keys(mutableCommentMap).sort();
 
   for (const key of keys) {
     const title = `#### ${key[0].toUpperCase()}${key.slice(1).toLowerCase()}\n`;
 
-    const values = commentMap[key];
+    const values = mutableCommentMap[key];
     const list = `${values.map(v => `- ${v}`).join('\n')}\n\n`;
 
     notes += `${title}${list}`;
   }
 
-  return notes ? `# Notes\n\n${notes}` : '';
+  notes = notes ? `# Notes\n\n${notes}` : '';
+
+  fs.writeFileSync(outputFile, notes);
 }
 
-async function main() {
+function main() {
   try {
-    const commentMap: { [k: string]: string[] } = {};
     const dirPaths = ['./', '../client', '../server', '../common'];
+    const outputFile = 'NOTES.md';
+    const mutableCommentMap: { [k: string]: string[] } = {};
 
     // @notes[notes] Notes are generated off of any comment that starts with `@notes[NAMESPACE]`
     // @notes[notes] Namespaces are there to group related comments under one title
 
     for (const dir of dirPaths) {
       const dirPath = path.join(__dirname, dir);
-      await getDirContents(dirPath, commentMap);
+      getDirContentsRecursively(dirPath, mutableCommentMap);
       console.log(`    -> completed: ${dirPath}`);
     }
 
-    const notes = createNotes(commentMap);
-    if (notes) fs.writeFileSync('NOTES.md', notes);
+    writeNotesToFile(outputFile, mutableCommentMap);
   } catch (e) {
     console.error(e);
   }
 }
 
-(async () => {
+(() => {
   try {
-    await main();
+    main();
     console.log('\n  ✅ Notes completed!\n');
   } catch (e) {
     console.error(e);
